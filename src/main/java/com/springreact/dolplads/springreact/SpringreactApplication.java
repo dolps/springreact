c1e80ea32c9583f73df469258882d69e615a73f8
dolps <dolplads@gmail.com>
2017-06-07 20:54:13 +0200

    still initing project

--- src/main/java/com/springreact/dolplads/springreact/SpringreactApplication.java
+++ src/main/java/com/springreact/dolplads/springreact/SpringreactApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringreactApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringreactApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringreactApplication.class, args);
+    }
 }
